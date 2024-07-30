a8f9e1590d4a8229f9e0939b59bfdd2eacfd06a6
ssoto83 <168875470+ssoto83@users.noreply.github.com>
2024-07-30 16:09:15 -0400

    all shapes tested and generating logos

--- index.js
+++ index.js
@@ -14,23 +14,35 @@ function validateS(shape) {
 }
 
 // To create the SVG file
-function createSVG(text, textColor, shape, shapeColor) {
+function createSVG(text, textColor, shape, shapeColor,) {
     let shapeElement;
-
+  
     if (shape === 'circle') {
         shapeElement = `<circle cx="150" cy="100" r="80" fill="${shapeColor}" />`;
     } else if (shape === 'triangle') {
-        shapeElement = `<polygon points ="100, 200 200,200 150,100" fill="${shapeColor}"/>`;
+        shapeElement = `<polygon points ="100, 200 200,200 150,100" fill="${shapeColor}" />`;
     } else if (shape === 'square') {
-        shapeElement = `<rect x="125" y="125" width="50" fill="${shapeColor}" />`;
+        shapeElement = `<rect x="75" y="75" width="150" height ="150" fill="${shapeColor}" />`;
     } else {
         shapeElement = '';
     }
+    // Adjust text positioning
+    let textX = 150;
+    let textY = 100;
 
+    if (shape === 'triangle') {
+        textY = 180;
+    } else if (shape === 'square') {
+        textY = 150;
+    } else if (shape === 'circle'){
+        textY = 110;
+    }
+
+   
     const svgContent = `<svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
-    <text x="150" y="100" font-size="40" text-anchor="middle" fill="${textColor}">${text}</text>
-    ${shapeElement}
-</svg>`;
+    ${shapeElement} <!-- render the shape element first -->
+    <text x="${textX}" y="${textY}" font-size="34" text-anchor="middle" fill="${textColor}">${text}</text> <!-- render the text on top -->
+    </svg>`;
 
     fs.writeFileSync('logo.svg', svgContent, 'utf-8');
     console.log('Generated logo.svg');
